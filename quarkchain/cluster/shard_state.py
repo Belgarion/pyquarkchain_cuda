tim-yoshi/pyquarkchain

add ENABLE_EVM_TIMESTAMP config

quarkchain/cluster/shard_state.py
@@ -292,6 +292,14 @@ class ShardState:
                 )
             )
 
+        # Check if EVM is disabled
+        if (
+            evm_tx.to == b"" and
+            self.env.quark_chain_config.ENABLE_EVM_TIMESTAMP is not None and
+            evm_state.timestamp < self.env.quark_chain_config.ENABLE_EVM_TIMESTAMP
+        ):
+            raise RuntimeError("smart contract creation is not allowed before evm is enabled")
+
         # This will check signature, nonce, balance, gas limit
         validate_transaction(evm_state, evm_tx)
 
@@ -999,6 +1007,15 @@ class ShardState:
                 ] + 1 > xshard_tx_limits.get(evm_tx.to_full_shard_id, 0):
                     poped_txs.append(evm_tx)  # will be put back later
                     continue
+
+            # Check if EMV is disabled
+            if (
+                evm_tx.to == b"" and
+                self.env.quark_chain_config.ENABLE_EVM_TIMESTAMP is not None and
+                block.header.create_time < self.env.quark_chain_config.ENABLE_EVM_TIMESTAMP
+            ):
+                # Drop the smart contract creation tx from tx_queue
+                continue
 
             try:
                 tx = Transaction(code=Code.create_evm_code(evm_tx))

quarkchain/cluster/tests/test_shard_state.py
@@ -6,6 +6,7 @@ from quarkchain.cluster.shard_state import ShardState
 from quarkchain.cluster.tests.test_utils import (
     get_test_env,
     create_transfer_transaction,
+    create_contract_creation_transaction,
 )
 from quarkchain.config import ConsensusType
 from quarkchain.core import CrossShardTransactionDeposit, CrossShardTransactionList
@@ -1617,3 +1618,36 @@ class TestShardState(unittest.TestCase):
         m = state.get_tip().create_block_to_append(address=acc, difficulty=1000)
         with self.assertRaises(ValueError):
             state.finalize_and_add_block(m)
+
+    def test_enable_evm_timestamp(self):
+        id1 = Identity.create_random_identity()
+        acc1 = Address.create_from_identity(id1, full_shard_key=0)
+        acc2 = Address.create_random_account(full_shard_key=0)
+
+        env = get_test_env(genesis_account=acc1, genesis_minor_quarkash=10000000)
+        state = create_default_shard_state(env=env)
+
+        # Add a root block to have all the shards initialized
+        root_block = state.root_tip.create_block_to_append().finalize()
+        state.add_root_block(root_block)
+
+        tx = create_contract_creation_transaction(
+            shard_state=state,
+            key=id1.get_key(),
+            from_address=acc1,
+            to_full_shard_key=0,
+        )
+        self.assertTrue(state.add_tx(tx))
+
+        b1 = state.create_block_to_mine()
+        self.assertEqual(len(b1.tx_list), 1)
+
+        env.quark_chain_config.ENABLE_EVM_TIMESTAMP = b1.header.create_time + 100
+        b2 = state.create_block_to_mine()
+        self.assertEqual(len(b2.tx_list), 0)
+
+        with self.assertRaisesRegexp(
+            RuntimeError,
+            "smart contract creation is not allowed before evm is enabled"
+        ):
+            state.finalize_and_add_block(b1)

Belgarion/pyquarkchain_cuda

quarkchain/cluster/shard_state.py
@@ -451,6 +451,14 @@ class ShardState:
         if evm_tx.is_cross_shard and evm_tx.startgas > xshard_gas_limit:
             raise RuntimeError("xshard evm tx exceeds xshard gas limit")
 
+        # Check if EVM is disabled
+        if (
+            self.env.quark_chain_config.ENABLE_EVM_TIMESTAMP is not None and
+            evm_state.timestamp < self.env.quark_chain_config.ENABLE_EVM_TIMESTAMP
+        ):
+            if evm_tx.to == b"" or evm_tx.data != b"":
+                raise RuntimeError("smart contract tx is not allowed before evm is enabled")
+
         # This will check signature, nonce, balance, gas limit
         validate_transaction(evm_state, evm_tx)
 
@@ -1134,6 +1142,16 @@ class ShardState:
             to_branch = Branch(evm_tx.to_full_shard_id)
 
             tx = TypedTransaction(SerializedEvmTransaction.from_evm_tx(evm_tx))
+
+            # Check if EMV is disabled
+            if (
+                self.env.quark_chain_config.ENABLE_EVM_TIMESTAMP is not None and
+                block.header.create_time < self.env.quark_chain_config.ENABLE_EVM_TIMESTAMP
+            ):
+                if evm_tx.to == b"" or evm_tx.data != b"":
+                    # Drop the smart contract creation tx from tx_queue
+                    continue
+
             try:
                 apply_transaction(evm_state, evm_tx, tx.get_hash())
                 block.add_tx(tx)

quarkchain/cluster/tests/test_shard_state.py
@@ -6,6 +6,7 @@ from quarkchain.cluster.shard_state import ShardState
 from quarkchain.cluster.tests.test_utils import (
     get_test_env,
     create_transfer_transaction,
+    create_contract_creation_transaction,
 )
 from quarkchain.config import ConsensusType
 from quarkchain.core import CrossShardTransactionDeposit, CrossShardTransactionList
@@ -2223,4 +2224,73 @@ class TestShardState(unittest.TestCase):
         self.assertEqual(len(b2.tx_list), 0)
 
         with self.assertRaisesRegexp(ValueError, "tx_list should be empty before tx is enabled"):
+            state.finalize_and_add_block(b1)
+
+    def test_enable_evm_timestamp_with_contract_create(self):
+        id1 = Identity.create_random_identity()
+        acc1 = Address.create_from_identity(id1, full_shard_key=0)
+        acc2 = Address.create_random_account(full_shard_key=0)
+
+        env = get_test_env(genesis_account=acc1, genesis_minor_quarkash=10000000)
+        state = create_default_shard_state(env=env)
+
+        # Add a root block to have all the shards initialized
+        root_block = state.root_tip.create_block_to_append().finalize()
+        state.add_root_block(root_block)
+
+        tx = create_contract_creation_transaction(
+            shard_state=state,
+            key=id1.get_key(),
+            from_address=acc1,
+            to_full_shard_key=0,
+        )
+        self.assertTrue(state.add_tx(tx))
+
+        b1 = state.create_block_to_mine()
+        self.assertEqual(len(b1.tx_list), 1)
+
+        env.quark_chain_config.ENABLE_EVM_TIMESTAMP = b1.header.create_time + 100
+        b2 = state.create_block_to_mine()
+        self.assertEqual(len(b2.tx_list), 0)
+
+        with self.assertRaisesRegexp(
+            RuntimeError,
+            "smart contract tx is not allowed before evm is enabled"
+        ):
+            state.finalize_and_add_block(b1)
+
+    def test_enable_evm_timestamp_with_contract_call(self):
+        id1 = Identity.create_random_identity()
+        acc1 = Address.create_from_identity(id1, full_shard_key=0)
+        acc2 = Address.create_random_account(full_shard_key=0)
+
+        env = get_test_env(genesis_account=acc1, genesis_minor_quarkash=10000000)
+        state = create_default_shard_state(env=env)
+
+        # Add a root block to have all the shards initialized
+        root_block = state.root_tip.create_block_to_append().finalize()
+        state.add_root_block(root_block)
+
+        tx = create_transfer_transaction(
+            shard_state=state,
+            key=id1.get_key(),
+            from_address=acc1,
+            to_address=acc2,
+            value=12345,
+            gas=50000,
+            data=b"1234",
+        )
+        self.assertTrue(state.add_tx(tx))
+
+        b1 = state.create_block_to_mine()
+        self.assertEqual(len(b1.tx_list), 1)
+
+        env.quark_chain_config.ENABLE_EVM_TIMESTAMP = b1.header.create_time + 100
+        b2 = state.create_block_to_mine()
+        self.assertEqual(len(b2.tx_list), 0)
+
+        with self.assertRaisesRegexp(
+            RuntimeError,
+            "smart contract tx is not allowed before evm is enabled"
+        ):
             state.finalize_and_add_block(b1)